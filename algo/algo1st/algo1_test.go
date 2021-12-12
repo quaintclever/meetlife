eea2f0cf034471d18c08a5bbe20d822a2cb1cc9b
quaint <857776215@qq.com>
2021-12-12 14:05:11 +0800

    :memo: leetcode clock in

--- algo/algo1st/algo1_test.go
+++ algo/algo1st/algo1_test.go
@@ -1,1 +1,18 @@
 package algo1st
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestToLowerCase(t *testing.T) {
+	testStr := "Hello World!"
+
+	got := toLowerCase(testStr)
+	want := strings.ToLower(testStr)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Error("error")
+	}
+}
--- algo/algo1st/alog1.go
+++ algo/algo1st/alog1.go
@@ -1,1 +1,12 @@
 package algo1st
+
+// 709. 转换成小写字母
+func toLowerCase(str string) string {
+	ans := []byte(str)
+	for i := 0; i < len(str); i++ {
+		if ans[i] >= 65 && ans[i] <= 90 {
+			ans[i] += 32
+		}
+	}
+	return string(ans)
+}
