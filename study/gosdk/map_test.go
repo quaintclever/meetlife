725e9cbbb31afcd08e16f1f55c00f3d20585efcc
quaintclever <857776215@qq.com>
2022-01-21 11:27:33 +0800

    :memo: map test

--- study/gosdk/map_test.go
+++ study/gosdk/map_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMapInt(t *testing.T) {
+func TestMapString(t *testing.T) {
 	// 测试 map string 零值
 	m1 := make(map[string]string, 10)
 	fmt.Printf("step1:map:%v\n", m1)
@@ -16,9 +16,10 @@ func TestMapInt(t *testing.T) {
 	fmt.Println(m1["test2"])
 	fmt.Println(m1["hello"])
 	fmt.Println(m1["hello"] == "")
+}
 
+func TestMapInt(t *testing.T) {
 	fmt.Println("====================================")
-
 	// 测试 map int 零值
 	m0 := make(map[int]int, 10)
 	fmt.Printf("step1:map:%v\n", m0)
@@ -28,4 +29,15 @@ func TestMapInt(t *testing.T) {
 	fmt.Println(m0[0])
 	fmt.Println(m0[1])
 	fmt.Println(m0[10])
+	fmt.Println("====================================")
+	m1 := map[string]int{
+		"test1": 1,
+		"test2": 2,
+	}
+	fmt.Println(m1["test1"] != 1)
+	fmt.Println(m1["test1"])
+	fmt.Println(m1["test2"] != 1)
+	fmt.Println(m1["test2"])
+	fmt.Println(m1["test3"] != 1)
+	fmt.Println(m1["test3"])
 }
