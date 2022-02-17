03a1edb199301c5e30e5d49c822b12a5ace1228b
quaintclever <857776215@qq.com>
2022-02-17 10:37:59 +0800

    :memo: string test

--- livetl/str/genstr_test.go
+++ livetl/str/genstr_test.go
@@ -2,10 +2,45 @@ package str
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestGenRandomStr(t *testing.T) {
-	str := genRandomStr(10)
+	str := GenRandomStr(10)
 	fmt.Println(str)
+	fmt.Println("======================")
+	str2 := GenTimeRandStr(TestType)
+	fmt.Println(str2)
+}
+
+type FlagType string
+
+const (
+	TestType FlagType = "T"
+)
+
+func GenTimeRandStr(flag FlagType) string {
+	// 运维单 session 生成
+	now := time.Now()
+	rand.Seed(now.UnixNano())
+	rn := rand.Intn(1000)
+	mixSession := fmt.Sprintf("%s%s%03d", flag, now.Format("20060102150405"), rn)
+	return mixSession
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+// 生成随机字符串
+func GenRandomStr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
