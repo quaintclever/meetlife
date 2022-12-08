ca21996beadbe1d156d495d974ee39f76f8ccba3
quaint <857776215@qq.com>
2022-12-09 01:08:41 +0800

    :memo: rpc test.

--- base/s_net/rpc_test.go
+++ base/s_net/rpc_test.go
@@ -1,12 +1,51 @@
 package snet
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"testing"
+)
 
-func TestRpcClient(t *testing.T) {
+type Args struct {
+	A, B int
+}
 
-	// rpc.Register()
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
 }
 
 func TestRpcServer(t *testing.T) {
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("err :%v", err)
+	}
+	http.Serve(l, nil)
+}
 
+func TestRpcClient(t *testing.T) {
+	c, err := rpc.DialHTTP("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalf("err: %v", err.Error())
+	}
+
+	args := &Args{
+		A: 2,
+		B: 3,
+	}
+	var rs int
+
+	err2 := c.Call("Arith.Multiply", args, &rs)
+	if err2 != nil {
+		log.Fatalf("err: %v", err2.Error())
+	}
+	fmt.Printf("Arith: %d * %d = %d \n", args.A, args.B, rs)
 }
